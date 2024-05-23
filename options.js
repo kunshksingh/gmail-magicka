126e7cb9f0b641dd8fe1f6300588b87c5725bbf7
kunshksingh <kunshksingh@gmail.com>
2024-05-23 12:48:32 +0530

    UserName storage fix

--- options.js
+++ options.js
@@ -1,8 +1,11 @@
 document.getElementById('settings-form').addEventListener('submit', function(e) {
     e.preventDefault();
+    const userName = document.getElementById('userName').value;
     const apiKey = document.getElementById('apiKey').value;
+    chrome.storage.sync.set({userName: userName}, function () {
+        
+    });
     chrome.storage.sync.set({apiKey: apiKey}, function() {
-        console.log('API Key saved: ' + apiKey);
-        // Optionally, display a message to the user that the API key was saved.
+
     });
 });
